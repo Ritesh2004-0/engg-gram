e6ff71b7961c18a89bad28783e1f07d6f3e93361
Your Name <Ritesh>
2026-06-07 02:45:10 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://engg-gram.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
