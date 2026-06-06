b3d80bc1ca9a93f83b4f477f684ad2c4e85f999c
Your Name <Ritesh>
2026-06-07 01:44:37 +0530

    Removed uploads mount for Render

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 from app.routes.note_routes import router as note_router
 from app.routes.admin_routes import router as admin_router
 
 app = FastAPI()
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,21 +14,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Static uploads folder
-app.mount(
-    "/uploads",
-    StaticFiles(directory="app/uploads"),
-    name="uploads"
-)
-
-# Routes
 app.include_router(admin_router)
 app.include_router(note_router)
 
-
 @app.get("/")
 def root():
-
     return {
         "message": "DBATU Notes API Running Successfully"
     }
